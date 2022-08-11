f8c8dbf4aefb00dadf1486869dd4834f7474028d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-11 11:38:24 +0600

    Added Assignment-4 Problem-1

---
+++ radianToDegree.js
@@ -0,0 +1,8 @@
+function radianToDegree(radian) {
+    const degree = radian * 57.2958;
+    return degree;
+}
+
+const inputRadiun = 2;
+const resultDegree = radianToDegree(inputRadiun);
+console.log(resultDegree);
