306df779bd2a189bbb28001b525bb56ea9944380
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-12 13:41:47 +0600

    Update all

--- radianToDegree.js
+++ radianToDegree.js
@@ -1,15 +1,9 @@
 function radianToDegree(radian) {
 
-    if (typeof radian === "string") {
-        return 'Please! Give an Integer Or Float value'
-
-    }
-    else {
-        const degree = radian * 57.2958;
+    const degree = radian * 57.2958;
         return parseFloat(degree.toFixed(2));
-    }
 }
 
-const inputRadiun = '2';
+const inputRadiun = '3';
 const resultDegree = radianToDegree(inputRadiun);
 console.log(resultDegree);
